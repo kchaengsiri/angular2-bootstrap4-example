87b8d1fbc9ea9f23c56f2ce1a0aca35f8128975d
Kitisak Chaengsiri <chaengsiri.k@gmail.com>
2017-01-26 01:56:12 +0700

    first commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Angular 2 with a Webpack
+
+### commands
+
+```
+    `npm start` for run dev server
+    `npm run test` for run karma test
+    `npm run build` for run webpack bundle build
+```
---
+++ src/vendor.ts
@@ -0,0 +1,21 @@
+// Angular
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+
+// RxJS
+import 'rxjs';
+
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbootstrap/css/mdb.min.css';
+
+import 'jquery';
+import 'tether';
+import 'bootstrap';
+import 'mdbootstrap';
